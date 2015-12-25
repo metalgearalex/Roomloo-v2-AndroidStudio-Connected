8b070a1a5188580e7afbfa49f141e60c48d8d234
metalgearalex <alex.chavdaroff@gmail.com>
2015-12-25 16:14:09 -0500

    formatted text in the individual apartment view

--- app/src/main/java/com/example/alex/roomloo_v2/ApiConnector.java
+++ app/src/main/java/com/example/alex/roomloo_v2/ApiConnector.java
@@ -169,7 +169,11 @@ public Apartment getApartment(int idInt) {
                 Integer idInt = apartmentJsonObject.getInt("id");
                 Apartment apartment = new Apartment(idInt); //UUID from String already converts our JSON string result into a UUID
 
-                apartment.setApartmentText(apartmentJsonObject.getInt("price") + " " + apartmentJsonObject.getInt("bedrooms") + " " + apartmentJsonObject.getInt("bathrooms") );
+                //done to format the price of the apartment to have a comma separator
+                int apartmentPrice = apartmentJsonObject.getInt("price");
+                String apartmentPriceText = String.format("%,d", apartmentPrice);
+
+                apartment.setApartmentText("Price: $" + apartmentPriceText + " " + "Bedrooms: " + apartmentJsonObject.getInt("bedrooms") + " " + "Bathrooms: " + apartmentJsonObject.getInt("bathrooms") );
                 apartment.setApartmentLatitude(apartmentJsonObject.getJSONObject("building").getDouble("latitude"));
                 apartment.setApartmentLongitude(apartmentJsonObject.getJSONObject("building").getDouble("longitude"));
 
